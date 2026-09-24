79f3f74e80052d3cf7467a46411d4c6e37a4f419
ayaayatee741-sudo <ayaayatee741@gmail.com>
2026-09-24 20:32:49 +0800

    Completed Prelogin page layout styles and HTML elements

--- app.py
+++ app.py
@@ -668,3 +668,7 @@ if __name__ == '__main__':
     print()
     
     app.run(debug=True, host='0.0.0.0', port=5000)
+
+@app.route('/api/logs')
+def get_logs():
+    return jsonify({"message": "Connected to app.py!"})
